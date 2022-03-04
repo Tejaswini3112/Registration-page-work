6ade8e75799b7697372eed33081055eb9c412289
saurabh-16092001 <75741538+saurabh-16092001@users.noreply.github.com>
2022-03-04 17:38:57 +0530

    what you did

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Regform from "./components/Regform"
+import './App.css';
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+     <Regform></Regform>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div className="block bg-black text-white mx-auto ">
+
+
+        <div className="flex h-28">
+
+            <div className="p-5  m-auto">
+
+                <a href="/">Terms & Conditions Privacy Policy</a>
+                &copy;
+            </div>
+            <div className=" text-right m-auto">
+                <a href="/">Follow us</a>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default footer
